39887f3786eca3f922be6c1dc25a8b47ad2af883
Dawid Kossowski <dawidkossowski@gmail.com>
2018-02-08 20:43:26 +0100

    orders model

---
+++ java/src/main/java/orders/Orders.java
@@ -0,0 +1,37 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+
+}
---
+++ java/src/main/java/orders/OrdersRepository.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+public interface OrdersRepository extends CrudRepository<hello.Orders, Long> {
+
+}
