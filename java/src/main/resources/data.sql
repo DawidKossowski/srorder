c987f83aa844db493a0226eb05ab7f6c628abcef
AdaSo <adajedrzejczak@o2.pl>
2018-02-18 00:29:36 +0100

    User ex.

--- java/src/main/resources/data.sql
+++ java/src/main/resources/data.sql
@@ -2,3 +2,12 @@ INSERT INTO `product` (name, price, amount) VALUES ('kaszanka', 69, 100);
 INSERT INTO `product` (name, price, amount) VALUES ('marchewka', 96, 1);
 INSERT INTO `product` (name, price, amount) VALUES ('piguly', 666, 69);
 INSERT INTO `product` (name, price, amount) VALUES ('kielbassa', 32, 0);
+
+
+INSERT INTO `adress` (adress) VALUE ('ChIJTfDDb82e4jARBGCJMk2jOL4');
+INSERT INTO `adress` (adress) VALUE ('EiZBbGVqZSBKZXJvem9saW1za2llLCBQcnVzemvDs3csIFBvbHNrYQ');
+
+
+
+INSERT INTO `user` (sex, name, surname, email, password) VALUES ('male', 'Adam', 'Małysz', 'adam.malysz@gmail.com', 'bananbulka');
+INSERT INTO `user_adress` (adress_id, user_id) VALUES (2, 1);
